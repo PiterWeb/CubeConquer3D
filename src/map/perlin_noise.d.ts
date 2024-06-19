841a3ce260ddd5c9a686cf7a971b86f4b2e63df1
PiterWeb <piterzdev@gmail.com>
2024-06-19 15:00:47 +0200

    Docs perlin noise

--- src/map/perlin_noise.d.ts
+++ src/map/perlin_noise.d.ts
@@ -1,9 +1,15 @@
 declare global {
+    /** Perlin noise helper functions */
     var noise: {
+        /** Give a seed to the noise generator */
         seed: (seed: number) => void;
+        /** 2D simplex noise */
         simplex2: (x: number, y: number) => number;
+        /** 3D simplex noise */
         simplex3: (x: number, y: number, z: number) => number;
+        /** 2D perlin noise */
         perlin2: (x: number, y: number) => number;
+        /** 3D perlin noise */
         perlin3: (x: number, y: number, z: number) => number;
     };
 }
