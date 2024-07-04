d3a44775d461561b5eea5189bb19ff46c6191839
PiterWeb <piterzdev@gmail.com>
2024-07-04 14:21:09 +0200

    fall fixed

--- src/entities/player/raycast.js
+++ src/entities/player/raycast.js
@@ -55,7 +55,7 @@ export default class Raycast {
      * @param {number} distance Default value is 1.5 to avoid the player to fall when moving ()
      */
     boxHaveToFall(direction, distance = 1.5) {
-        if (this.#player.position.y <= yOrigin) return;
+        if (this.#player.position.y <= yOrigin) return false;
         const nextPlayerPosition = this.#player.position.clone();
 
         //Move the player to the new direction to check if there is a block below
